eeed8fa4c271c4601535679bae4d5bbfe2992a4e
Farid <Blackwazz7@gmail.com>
2022-11-19 23:23:08 +0500

    Start

---
+++ Assets/Scripts/ComboCounter/ComboCounter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public sealed class ComboCounter : MonoBehaviour
+{
+    [SerializeField] private CountView _view;
+    private int _value;
+
+    public void Increase()
+    {
+        _value++;
+        _view.Visualize(_value);
+    }
+
+    public void ResetToZero()
+    {
+        _value = 0;
+        _view.Visualize(_value);
+    }
+}
