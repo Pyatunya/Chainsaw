36180553af0259e2165148169cdb17e2d4ab6a78
GigaOrts <ortsortskhoev95@gmail.com>
2022-11-19 11:20:55 +0300

    base options

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,17 @@
+using System;
+using UnityEngine;
+
 public class Player : Entity
 {
+    public void Attack()
+    {
+        
+    }
+
+    public void Dash()
+    {
+
+    }
+
 
 }
--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,6 +1,16 @@
+using System;
 using UnityEngine;
 
 public class PlayerInputController : MonoBehaviour
 {
-    
+    [SerializeField] private Player _player;
+    [SerializeField] private KeyCode _key;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(_key))
+        {
+            _player.Attack();
+        }
+    }
 }
