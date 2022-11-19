51e0775fa729a6bcbd4f75c767ceb0223a5d8a2e
Farid <Blackwazz7@gmail.com>
2022-11-19 15:12:18 +0500

    Start Spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,6 +1,25 @@
+using System.Collections;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class EnemySpawner : MonoBehaviour
+public sealed class EnemySpawner : MonoBehaviour
 {
-    
+    [SerializeField, Min(0.1f)] private float _spawnSeconds = 1.5f;
+    [SerializeField] private Entity[] _prefabs;
+    [SerializeField] private float _radius = 1.5f;
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnSeconds);
+            var prefab = _prefabs[Random.Range(0, _prefabs.Length)];
+            Instantiate(prefab, Random.insideUnitCircle * _radius, Quaternion.identity, transform);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(Color.magenta.r, Color.magenta.g, Color.magenta.b, 0.25f);
+    }
 }
