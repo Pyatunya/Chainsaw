6ba193a5136ae096b1b8cb0b2e321a6f8d2de587
Vasilii Zabolotskii <vazabsky@gmail.com>
2022-11-21 17:03:10 +0900

    changed charging time and removed GetDashForceCoefficient method

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,7 +6,7 @@ public class Player : Entity
 {
     private float _moveForce = 500f;
     private float _dashForce = 2800f;
-    private float _maxTimeForDashForce = 3f;
+    private float _maxTimeForDashForce = 1.2f;
 
     private TargetSearcher _targetSearcher;
     private Rigidbody2D _rigidbody;
@@ -31,7 +31,6 @@ public class Player : Entity
         {
             Dashing?.Invoke();
             float chargedDashForce = GetChargedDashForce(chargingTimeForDashForce);
-            Debug.Log(chargedDashForce);
             MoveTo(closest, chargedDashForce);
         }
     }
@@ -42,15 +41,11 @@ public class Player : Entity
         _rigidbody.AddForce(direction * force);
     }
 
-    private float GetChargedDashForce(float chargingtime)
+    private float GetChargedDashForce(float chargingTime)
     {
-        float deltaForce = ((_dashForce - _moveForce) * GetDashForceCoefficient(chargingtime, _maxTimeForDashForce));
+        float dashForceCoefficient = Mathf.Min(chargingTime, _maxTimeForDashForce) / _maxTimeForDashForce;
+        float deltaForce = (_dashForce - _moveForce) * dashForceCoefficient;
         float result = deltaForce + _moveForce;
         return result;
-    }
-
-    private float GetDashForceCoefficient(float chargingTime, float maxTime)
-    {
-        return Mathf.Min(chargingTime, maxTime) / maxTime;
     }
 }
