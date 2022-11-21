dc72e3330d3e0c2c562614474e68c4a800da0786
Farid <Blackwazz7@gmail.com>
2022-11-21 19:10:30 +0500

    Int Utils

--- Assets/Scripts/ComboCounter/ComboCounter.cs
+++ Assets/Scripts/ComboCounter/ComboCounter.cs
@@ -1,14 +1,26 @@
-﻿using UnityEngine;
+﻿using Tools;
+using UnityEngine;
 
 public sealed class ComboCounter : MonoBehaviour
 {
+    [SerializeField] private Wallet _wallet;
     [SerializeField] private CountView _view;
+    [SerializeField] private int _countWhenNeedAddNewMoney = 5;
+    [SerializeField] private int _addMoney = 10;
+    
     private int _value;
 
     public void Increase()
     {
         _value++;
         _view.Visualize(_value);
+
+        var numberOfWholeDivisions = _value.GetNumberOfWholeDivisions(5);
+        
+        if (numberOfWholeDivisions > 0)
+        {
+            _wallet.Put(numberOfWholeDivisions * _addMoney);
+        }
     }
 
     public void ResetToZero()
---
+++ Assets/Scripts/Tools/IntUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tools
+{
+    public static class IntUtils
+    {
+        public static int GetNumberOfWholeDivisions(this int number, int divider)
+        {
+            var count = 0;
+
+            if (number < divider)
+                throw new ArgumentOutOfRangeException(nameof(divider));
+            
+            if (number <= 0) 
+                throw new ArgumentOutOfRangeException(nameof(number));
+            
+            if (divider <= 0) 
+                throw new ArgumentOutOfRangeException(nameof(divider));
+
+            while (number % divider == 0)
+            {
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
