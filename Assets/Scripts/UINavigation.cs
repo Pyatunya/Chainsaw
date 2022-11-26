4a0a240f3ae1407808c010c7c10b1ccdb86c5004
Farid <Blackwazz7@gmail.com>
2022-11-26 23:42:45 +0500

    Fixed Time Scale

--- Assets/Scripts/UINavigation.cs
+++ Assets/Scripts/UINavigation.cs
@@ -1,69 +1,62 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public  sealed class UINavigation : MonoBehaviour
+public sealed class UINavigation : MonoBehaviour
 {
     [SerializeField] private Image _posych;
     [SerializeField] private List<Image> _uiElements;
     [SerializeField] private float _needTimeForClick = 0.5f;
-    
+
     private int _index;
     private float _time;
 
     private void Start()
     {
         _posych.transform.position = _uiElements[0].transform.position;
-        StartCoroutine(HandInput());
     }
 
     public void Add(Image element) => _uiElements.Add(element);
 
-    private IEnumerator HandInput()
+    private void Update()
     {
-        while (true)
+        if (Input.GetKey(KeyCode.F))
         {
-            if (Input.GetKey(KeyCode.F))
-            {
-                _time += Time.deltaTime;
-            }
-        
-            if (Input.GetKeyUp(KeyCode.F))
-            {
-                if (_time >= _needTimeForClick)
-                {
-                    if (_uiElements[_index].gameObject.TryGetComponent(out Button button))
-                    {
-                        button.onClick.Invoke();
-                    }
-                }
-            
-                else
-                {
-                    _index = GetNextIndex(_index);
-                    _posych.transform.position = _uiElements[_index].transform.position;
-                }
+            _time += Time.unscaledDeltaTime;
+        }
 
-                _time = 0f;
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            if (_time >= _needTimeForClick)
+            {
+                if (_uiElements[_index].gameObject.TryGetComponent(out Button button))
+                {
+                    button.onClick.Invoke();
+                }
             }
 
-            yield return null;
+            else
+            {
+                _index = GetNextIndex(_index);
+                _posych.transform.position = _uiElements[_index].transform.position;
+            }
+
+            _time = 0f;
         }
     }
-    
+
     private int GetNextIndex(int currentIndex)
     {
         if (currentIndex == _uiElements.Count - 1)
         {
-           return 0;
+            return 0;
         }
-        
+
         else
         {
             if (_uiElements[currentIndex + 1].gameObject.activeInHierarchy == false)
                 return GetActiveElementIndex(currentIndex);
-            
+
             return currentIndex + 1;
         }
     }
@@ -72,13 +65,13 @@ public  sealed class UINavigation : MonoBehaviour
     {
         if (start == _uiElements.Count - 1)
             return 0;
-        
+
         if (_uiElements[start + 1].gameObject.activeInHierarchy == false)
         {
             start += 1;
             return GetActiveElementIndex(start);
         }
-        
+
         else
         {
             return start;
