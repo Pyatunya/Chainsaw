3a41d9918dae7b00a47c4836b0359783d8d11490
Farid <Blackwazz7@gmail.com>
2022-11-26 21:43:27 +0500

    Animation

--- Assets/Scripts/ComboCounter/ComboCounterView.cs
+++ Assets/Scripts/ComboCounter/ComboCounterView.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 public sealed class ComboCounterView : MonoBehaviour
 {
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private Animation _textShake;
     
     public void Visualize(int count)
     {
         _text.text = count.ToString();
 
+        if (count % 10 == 0)
+        {
+            _textShake.Play();
+        }
+        
         if (count == 0)
             _text.text = string.Empty;
     }
