e313e2026d689327dbc8332313d74693792eb0c5
Farid <Blackwazz7@gmail.com>
2022-11-30 21:05:36 +0500

    Wave System, Infinite Game

---
+++ Assets/Scripts/Level/Wave/ZombieWaveData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct ZombieWaveData
+{
+    [field: SerializeField] public Zombie[] Prefabs { get; private set; }
+    
+    [field: SerializeField] public float NeedTimeToStart { get; private set; }
+}
--- Assets/Scripts/Spawners/ZombieSpawner.cs
+++ Assets/Scripts/Spawners/ZombieSpawner.cs
@@ -1,31 +1,36 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public sealed class ZombieSpawner : EnemySpawner
 {
     [SerializeField] private Transform[] _spawnPoints;
-    [SerializeField] private Zombie[] _prefabs;
     [SerializeField] private LevelTimer _levelTimer;
     [SerializeField] private Score _score;
     [SerializeField] private ComboCounter _comboCounter;
     [SerializeField] private Player _player;
 
     private readonly Dictionary<Zombie, IndependentPool<Zombie>> _pools = new();
-    private const float SpeedOnHardLevelTime = 10f;
+    
+    public Zombie[] Prefabs { get; private set; }
 
-    private void Start()
+    public void SwitchPrefabs(Zombie[] prefabs)
     {
-        foreach (var prefab in _prefabs)
+        Prefabs = prefabs ?? throw new ArgumentNullException(nameof(prefabs));
+        
+        foreach (var prefab in prefabs)
         {
-            _pools.Add(prefab, new IndependentPool<Zombie>(new GameObjectsFactory<Zombie>(prefab, transform)));
+            if (_pools.ContainsKey(prefab) == false)
+                _pools.Add(prefab, new IndependentPool<Zombie>(new GameObjectsFactory<Zombie>(prefab, transform)));
         }
     }
 
     public override IEntity Create()
     {
-        var prefab = _prefabs[Random.Range(0, _prefabs.Length)];
+        var prefab = Prefabs[Random.Range(0, Prefabs.Length)];
         var zombie = _pools[prefab].Get();
-        zombie.Init(_player, _levelTimer.IsHardLevel ? SpeedOnHardLevelTime : 4f);
+        zombie.Init(_player, _levelTimer.GetSpeedFromCurrentTime());
 
         zombie.Init(_score, _comboCounter);
         zombie.transform.position = _spawnPoints[Random.Range(0, _spawnPoints.Length)].position;
