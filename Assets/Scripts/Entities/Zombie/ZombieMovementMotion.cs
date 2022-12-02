ebf273ca72a97750145bff08d9c2e68b0fc16b80
Farid <Blackwazz7@gmail.com>
2022-12-02 20:17:42 +0500

    CHANGS
    Co-Authored-By: Orts Ortskhoev <99609826+GigaOrts@users.noreply.github.com>

--- Assets/Scripts/Entities/Zombie/ZombieMovementMotion.cs
+++ Assets/Scripts/Entities/Zombie/ZombieMovementMotion.cs
@@ -1,31 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
-[RequireComponent(typeof(ZombieCollision), typeof(Zombie))]
+[RequireComponent(typeof(Zombie))]
 public sealed class ZombieMovementMotion : MonoBehaviour
 {
     [SerializeField] private float _returnMovementDistance = 5f;
-    private ZombieCollision _zombieCollision;
     private Zombie _zombie;
-
+    private Coroutine _returnMovement;
+    
     private void Start()
     {
-        _zombieCollision = GetComponent<ZombieCollision>();
         _zombie = GetComponent<Zombie>();
-        _zombieCollision.OnAttacked += OnAttacked;
+        _zombie.Health.OnDamaged += OnAttacked;
     }
 
     private void OnAttacked() => _zombie.StopMovement();
+
+    private void OnDestroy()
+    {
+        _zombie.Health.OnDamaged -= OnAttacked;
+    }
 
     private void Update()
     {
         if(_zombie.CanMove)
             return;
+
+        if(_returnMovement != null)
+            return;
         
-        var distance = (_zombie.Player.transform.position - transform.position).magnitude;
-        
-        if(distance >= _returnMovementDistance)
-        {
-            _zombie.ContinueMovement();
-        }
+        _returnMovement = StartCoroutine(TryReturnMovement());
+    }
+
+    private IEnumerator TryReturnMovement()
+    {
+        yield return new WaitForSeconds(0.75f);
+        _zombie.ContinueMovement();
+        _returnMovement = null;
     }
 }
