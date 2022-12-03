511276c0ffed14fa880a3752db818d111026a654
Farid <Blackwazz7@gmail.com>
2022-12-03 18:25:31 +0500

    Refactoring

--- Assets/Scripts/Spawners/EnemySpawnerRoot.cs
+++ Assets/Scripts/Spawners/EnemySpawnerRoot.cs
@@ -4,25 +4,32 @@ using Random = UnityEngine.Random;
 
 public sealed class EnemySpawnerRoot : MonoBehaviour
 {
-    [SerializeField] private EnemySpawner[] _spawners;
-
     private const float SpawnDelay = 2f;
     private const float SpawnSeconds = 0.45f;
 
-    public void StartSpawn()
-    {
-        StartCoroutine(Spawn());
-    }
+    public WaveData WaveData { get; private set; }
+    
+    public void StartSpawn() => StartCoroutine(Spawn());
 
+    public void SwitchWave(WaveData waveData)
+    {
+        WaveData = waveData;
+    }
+    
     private IEnumerator Spawn()
     {
         yield return new WaitForSeconds(SpawnDelay);
 
         while (true)
         {
-            yield return new WaitForSeconds(SpawnSeconds);
-            var spawner = _spawners[Random.Range(0, _spawners.Length)];
-            spawner.Create();
+            for (var i = 0; i < WaveData.EnemySpawnCountAtOnce; i++)
+            {
+                yield return new WaitForSeconds(SpawnSeconds);
+                var spawners = WaveData.Spawners;
+                var spawner = spawners[Random.Range(0, spawners.Length)];
+                spawner.Create();
+                
+            }
         }
     }
 }
