8bad4a3065da36c15c4c17244f3273adba8c9af8
Farid <Blackwazz7@gmail.com>
2022-12-03 17:12:11 +0500

    Done Attacks Cooldown

--- Assets/Scripts/Player/Input/PlayerAttackInput.cs
+++ Assets/Scripts/Player/Input/PlayerAttackInput.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public sealed class PlayerAttackInput : MonoBehaviour
 {
     [SerializeField] private PlayerChargingAttackInput[] _playerChargingAttackInputs;
+    [SerializeField, Min(0.01f)] private float _attackCooldown = 0.3f;
+    
     private const KeyCode Key = KeyCode.F;
-
     private Player _player;
     private bool _isDashCharging;
+    private float _time;
 
     private void Awake() => _player = GetComponent<Player>();
 
     private void Update()
     {
-        if (Input.GetKeyUp(Key))
+        _time += Time.deltaTime;
+        
+        if (Input.GetKeyUp(Key) && _time >= _attackCooldown)
         {
             if (_playerChargingAttackInputs.All(input => input.IsCharging == false))
             {
+                _time = 0;
                 _player.Attack();
             }
         }
